475e712332c40cc7f0c99e9405a39726c48674a0
bmuday <bmuday@live.fr>
2023-09-10 17:18:36 -0400

    deploy14

--- dom.js
+++ dom.js
@@ -1,30 +1,42 @@
 $(".members .member_image").each(function (index) {
-  $(this).attr(
-    "src",
-    `https://jsonplaceholder.typicode.com/photos/${index + 1}`
-  );
-});
-
-$(".articles .article_image").each(function (index) {
-  $(this).attr(
-    "src",
-    `https://jsonplaceholder.typicode.com/photos/${index + 6}`
-  );
-});
-
-$(".articles .article_text").each(async function (index) {
-  let post;
-  $.ajax({
+  const { thumbnailUrl } = $.ajax({
     type: "GET",
     url: `https://jsonplaceholder.typicode.com/photos/${index + 1}`,
     success: function (data) {
-      post = data;
-      console.log("post", data);
+      return data;
     },
     error: function (error) {
       console.log("FAIL....=================", error);
     },
   });
-  //   if (post.length > 50) post = post.substring(0, 50) + "...";
+  $(this).attr("src", thumbnailUrl);
+});
+
+$(".articles .article_image").each(function (index) {
+  const { thumbnailUrl } = $.ajax({
+    type: "GET",
+    url: `https://jsonplaceholder.typicode.com/photos/${index + 6}`,
+    success: function (data) {
+      return data;
+    },
+    error: function (error) {
+      console.log("FAIL....=================", error);
+    },
+  });
+  $(this).attr("src", thumbnailUrl);
+});
+
+$(".articles .article_text").each(async function (index) {
+  const { body } = $.ajax({
+    type: "GET",
+    url: `https://jsonplaceholder.typicode.com/posts/${index + 1}`,
+    success: function (data) {
+      return data;
+    },
+    error: function (error) {
+      console.log("FAIL....=================", error);
+    },
+  });
+  if (body.length > 50) body = body.substring(0, 50) + "...";
   $(this).text(post);
 });
