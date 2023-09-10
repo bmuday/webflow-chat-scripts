73e416858ea72deab2a5f6843108baa62733a886
bmuday <bmuday@live.fr>
2023-09-10 17:00:41 -0400

    deploy11

--- dom.js
+++ dom.js
@@ -1,17 +1,20 @@
 $(".members .member_image").each(function (index) {
-  $(this).attr("src", `https://jsonplaceholder.typicode.com/photos/${index}`);
+  $(this).attr(
+    "src",
+    `https://jsonplaceholder.typicode.com/photos/${index + 1}`
+  );
 });
 
 $(".articles .article_image").each(function (index) {
   $(this).attr(
     "src",
-    `https://jsonplaceholder.typicode.com/photos/${index + 5}`
+    `https://jsonplaceholder.typicode.com/photos/${index + 6}`
   );
 });
 
 $(".articles .article_text").each(async function (index) {
   const post = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${index}`
+    `https://jsonplaceholder.typicode.com/posts/${index + 1}`
   );
   if (post.length > 50) post = post.substring(0, 50) + "...";
   $(this).text(post);
