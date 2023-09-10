6e8c9775ae64ddb1b1f8b65dc8e48b6ecde6512e
bmuday <bmuday@live.fr>
2023-09-10 18:12:03 -0400

    deploy30

--- dom.js
+++ dom.js
@@ -1,14 +1,13 @@
-$(".members .member_image").each(function (index) {
-  let url;
-  $(document).ready(function () {
-    $.ajax({
+$(document).ready(function () {
+  $(".members .member_image").each(function (index) {
+    const url = $.ajax({
       url: `https://jsonplaceholder.typicode.com/photos/${index + 1}`,
       method: "GET",
       dataType: "json",
     })
       .done(function (res) {
         const data = res;
-        url = data.thumbnailUrl;
+        return data.thumbnailUrl;
       })
       .fail(function (error) {
         console.log(
@@ -18,16 +17,13 @@ $(".members .member_image").each(function (index) {
       .always(function () {
         console.log("Requête effectuée");
       });
+    console.log("this1", this);
+    console.log("url", url);
+    //   $(this).attr("src", url);
   });
-  console.log("this1", this);
-  console.log("url", url);
-  $(this).attr("src", url);
-});
 
-$(".articles .article_image").each(function (index) {
-  let url;
-  $(document).ready(function () {
-    $.ajax({
+  $(".articles .article_image").each(function (index) {
+    const url = $.ajax({
       url: `https://jsonplaceholder.typicode.com/photos/${index + 6}`,
       method: "GET",
       dataType: "json",
@@ -35,6 +31,7 @@ $(".articles .article_image").each(function (index) {
       .done(function (res) {
         const data = res;
         url = data.thumbnailUrl;
+        return url;
       })
       .fail(function (error) {
         console.log(
@@ -44,33 +41,32 @@ $(".articles .article_image").each(function (index) {
       .always(function () {
         console.log("Requête effectuée2");
       });
+    console.log("this2", this);
+    console.log("url", url);
+    //   $(this).attr("src", url);
   });
-  console.log("this2", this);
-  console.log("url", url);
-  $(this).attr("src", url);
-});
 
-$(".articles .article_text").each(async function (index) {
-  let body;
-  $.ajax({
-    url: `https://jsonplaceholder.typicode.com/posts/${index + 1}`,
-    method: "GET",
-    dataType: "json",
-  })
-    .done(function (res) {
-      const data = res;
-      body = data.body;
-      if (body.length > 20) body = body.substring(0, 20) + "...";
+  $(".articles .article_text").each(async function (index) {
+    const body = $.ajax({
+      url: `https://jsonplaceholder.typicode.com/posts/${index + 1}`,
+      method: "GET",
+      dataType: "json",
     })
-    .fail(function (error) {
-      console.log(
-        "La requête s'est terminée en échec. Infos : " + JSON.stringify(error)
-      );
-    })
-    .always(function () {
-      console.log("Requête effectuée3");
-    });
-  console.log("this3", this);
-  console.log("body", body);
-  $(this).text(body);
+      .done(function (res) {
+        const data = res;
+        if (body.length > 20) body = body.substring(0, 20) + "...";
+        return data.body;
+      })
+      .fail(function (error) {
+        console.log(
+          "La requête s'est terminée en échec. Infos : " + JSON.stringify(error)
+        );
+      })
+      .always(function () {
+        console.log("Requête effectuée3");
+      });
+    console.log("this3", this);
+    console.log("body", body);
+    //   $(this).text(body);
+  });
 });
