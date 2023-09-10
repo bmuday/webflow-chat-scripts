79352d09bf0acd0f24a0d1a2df77131d8f5d6245
bmuday <bmuday@live.fr>
2023-09-10 15:42:25 -0400

    first commit

---
+++ locales.js
@@ -0,0 +1,30 @@
+const locales = ["en", "fr"];
+const selectedLocale = locales[0];
+
+$("").on("click", function () {});
+
+const headerLinks = {
+  en: ["Rooms", "Members", "Blog", "Login", "Register"],
+  fr: ["Salons", "Membres", "Blog", "Se connecter", "S'inscrire"],
+};
+
+const form_placeholders = {
+  en: [
+    "Enter your username",
+    "Age",
+    "Male",
+    "Female",
+    "City",
+    "Enter",
+    "By logging in, you agree the terms",
+  ],
+  fr: [
+    "Entrez votre pseudo",
+    "Age",
+    "Homme",
+    "Femme",
+    "Ville",
+    "Entrez",
+    "En vous connectant, vous acceptez les CGU",
+  ],
+};
