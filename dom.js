ffecc2cc9c1f39c7adb8b5ce92067798e3bc8a11
bmuday <bmuday@live.fr>
2023-09-10 19:44:44 -0400

    deploy38

--- dom.js
+++ dom.js
@@ -1,65 +1,62 @@
 $(document).ready(function () {
-  $(".members .member_image").each(function (index) {
-    $.ajax({
-      url: `https://jsonplaceholder.typicode.com/photos/${index + 1}`,
-      method: "GET",
-      dataType: "json",
-    })
-      .done(function (data) {
-        console.log("data1", data);
-        console.log("this1", $(this));
-      })
-      .fail(function (error) {
-        console.log(
-          "La requête s'est terminée en échec. Infos : " + JSON.stringify(error)
-        );
-      })
-      .always(function () {
-        console.log("Requête effectuée");
+  $.ajax({
+    url: `https://jsonplaceholder.typicode.com/photos`,
+    method: "GET",
+    dataType: "json",
+  })
+    .done(function (data) {
+      console.log("data1", data);
+      $(".members .member_image").each(function (index) {
+        // $(this).attr('src', data)
       });
-    // $(this).attr("src", thumbnailUrl);
-  });
+    })
+    .fail(function (error) {
+      console.log(
+        "La requête s'est terminée en échec. Infos : " + JSON.stringify(error)
+      );
+    })
+    .always(function () {
+      console.log("Requête effectuée2");
+    });
 
-  $(".articles .article_image").each(function (index) {
-    $.ajax({
-      url: `https://jsonplaceholder.typicode.com/photos/${index + 6}`,
-      method: "GET",
-      dataType: "json",
-    })
-      .done(function (data) {
-        console.log("data2", data);
-        console.log("this2", $(this));
-      })
-      .fail(function (error) {
-        console.log(
-          "La requête s'est terminée en échec. Infos : " + JSON.stringify(error)
-        );
-      })
-      .always(function () {
-        console.log("Requête effectuée2");
+  $.ajax({
+    url: `https://jsonplaceholder.typicode.com/photos`,
+    method: "GET",
+    dataType: "json",
+  })
+    .done(function (data) {
+      console.log("data2", data);
+      $(".articles .article_image").each(function (index) {
+        // $(this).attr("src", thumbnailUrl);
       });
-    // $(this).attr("src", thumbnailUrl);
-  });
+    })
+    .fail(function (error) {
+      console.log(
+        "La requête s'est terminée en échec. Infos : " + JSON.stringify(error)
+      );
+    })
+    .always(function () {
+      console.log("Requête effectuée2");
+    });
 
-  $(".articles .article_text").each(function (index) {
-    $.ajax({
-      url: `https://jsonplaceholder.typicode.com/posts/${index + 1}`,
-      method: "GET",
-      dataType: "json",
-    })
-      .done(function (data) {
-        console.log("data3", data);
-        console.log("this3", $(this));
-      })
-      .fail(function (error) {
-        console.log(
-          "La requête s'est terminée en échec. Infos : " + JSON.stringify(error)
-        );
-      })
-      .always(function () {
-        console.log("Requête effectuée3");
+  $.ajax({
+    url: `https://jsonplaceholder.typicode.com/posts/${index + 1}`,
+    method: "GET",
+    dataType: "json",
+  })
+    .done(function (data) {
+      console.log("data3", data);
+      $(".articles .article_text").each(function (index) {
+        // if (body.length > 20) body = body.substring(0, 20) + "...";
+        // $(this).text(body);
       });
-    // if (body.length > 20) body = body.substring(0, 20) + "...";
-    // $(this).text(body);
-  });
+    })
+    .fail(function (error) {
+      console.log(
+        "La requête s'est terminée en échec. Infos : " + JSON.stringify(error)
+      );
+    })
+    .always(function () {
+      console.log("Requête effectuée3");
+    });
 });
