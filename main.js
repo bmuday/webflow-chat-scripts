11e33d1665940f2ef43cc6ac0186007ccde08597
bmuday <bmuday@live.fr>
2023-09-10 16:04:13 -0400

    deploy

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { createClient } from "@supabase/supabase-js";
+const { createClient } = require("@supabase/supabase-js");
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
