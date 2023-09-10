ff4fa7060f971c2d4a2f9dcd106c7280d6a1adbf
bmuday <bmuday@live.fr>
2023-09-10 17:33:12 -0400

    deploy15

--- dom.js
+++ dom.js
@@ -1,45 +1,67 @@
 $(".members .member_image").each(function (index) {
-  const data = $.ajax({
-    type: "GET",
-    url: `https://jsonplaceholder.typicode.com/photos/${index + 1}`,
-    success: function (data) {
-      return data;
-    },
-    error: function (error) {
-      console.log("FAIL....=================", error);
-    },
+  $(document).ready(function () {
+    $.ajax({
+      url: `https://jsonplaceholder.typicode.com/photos/${index + 1}`,
+      method: "GET",
+      dataType: "json",
+    })
+      .done(function (response) {
+        let data = JSON.stringify(response);
+        console.log("data1", data);
+        //   $(this).attr("src", thumbnailUrl);
+      })
+      .fail(function (error) {
+        console.log(
+          "La requête s'est terminée en échec. Infos : " + JSON.stringify(error)
+        );
+      })
+      .always(function () {
+        console.log("Requête effectuée");
+      });
   });
-  console.log("data", data);
-  //   $(this).attr("src", thumbnailUrl);
 });
 
 $(".articles .article_image").each(function (index) {
-  const data = $.ajax({
-    type: "GET",
-    url: `https://jsonplaceholder.typicode.com/photos/${index + 6}`,
-    success: function (data) {
-      return data;
-    },
-    error: function (error) {
-      console.log("FAIL....=================", error);
-    },
+  $(document).ready(function () {
+    $.ajax({
+      url: `https://jsonplaceholder.typicode.com/photos/${index + 6}`,
+      method: "GET",
+      dataType: "json",
+    })
+      .done(function (response) {
+        let data = JSON.stringify(response);
+        console.log("data2", data);
+        //   $(this).attr("src", thumbnailUrl);
+      })
+      .fail(function (error) {
+        console.log(
+          "La requête s'est terminée en échec. Infos : " + JSON.stringify(error)
+        );
+      })
+      .always(function () {
+        console.log("Requête effectuée2");
+      });
   });
-  console.log("data", data);
-  //   $(this).attr("src", thumbnailUrl);
 });
 
 $(".articles .article_text").each(async function (index) {
-  const data = $.ajax({
-    type: "GET",
+  $.ajax({
     url: `https://jsonplaceholder.typicode.com/posts/${index + 1}`,
-    success: function (data) {
-      return data;
-    },
-    error: function (error) {
-      console.log("FAIL....=================", error);
-    },
-  });
-  console.log("data", data);
-  //   if (body.length > 50) body = body.substring(0, 50) + "...";
-  //   $(this).text(body);
+    method: "GET",
+    dataType: "json",
+  })
+    .done(function (response) {
+      let data = JSON.stringify(response);
+      console.log("data3", data);
+      //   if (body.length > 50) body = body.substring(0, 50) + "...";
+      //   $(this).text(body);
+    })
+    .fail(function (error) {
+      console.log(
+        "La requête s'est terminée en échec. Infos : " + JSON.stringify(error)
+      );
+    })
+    .always(function () {
+      console.log("Requête effectuée3");
+    });
 });
