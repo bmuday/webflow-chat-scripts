48e1742a325d7d57dbb0bd26e11cc85e4ad3ae20
bmuday <bmuday@live.fr>
2023-09-10 22:01:07 -0400

    update register1

--- register.mjs
+++ register.mjs
@@ -2,4 +2,8 @@ $(document).ready(function () {
   $(".register_input").focus(function () {
     $(this).css("border", "1px solid #1991eb");
   });
+
+  $(".register_input").blur(function () {
+    $(this).css("border", "");
+  });
 });
