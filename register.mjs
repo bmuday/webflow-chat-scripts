a1d5212baaf95c6d39fbe0bce908cd77c8c0c330
bmuday <bmuday@live.fr>
2023-09-10 22:25:55 -0400

    update register2

--- register.mjs
+++ register.mjs
@@ -6,4 +6,26 @@ $(document).ready(function () {
   $(".register_input").blur(function () {
     $(this).css("border", "");
   });
+
+  async function createUser() {
+    const { data, error } = await supabase.auth.signUp({
+      email: "example@email.com",
+      password: "example-password",
+    });
+
+    console.log("data", data);
+    console.log("error", error);
+  }
+
+  $(".register_form").submit(async function (e) {
+    e.preventDefault();
+
+    const username = $("#username").val();
+    const email = $("#email").val();
+    const password = $("#password").val();
+
+    console.log("values", username, email, password);
+
+    await createUser(email, password);
+  });
 });
